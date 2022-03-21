68c679c5131afb163b00d140a6aacad3c5ddfb98
AdheenaBabu <babu.adheena.2001@gmail.com>
2022-03-21 15:39:29 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "Sample Text";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:  [
+          Text(text, style: TextStyle(fontFamily: "Poppins", fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.white, decoration: TextDecoration.none),),
+          const SizedBox(height: 15.0,),
+          ElevatedButton(
+              onPressed: (){},
+              child: const Text('Connect!'))
+        ],
+      ),
+    );
+  }
+}
